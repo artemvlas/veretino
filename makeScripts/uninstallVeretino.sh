44904a93b4e1f5d015a0fd5ac9ec62c50442bb18
Artem Vlasenko <124929134+artemvlas@users.noreply.github.com>
2023-02-19 15:40:40 +0200

    Add files via upload

---
+++ makeScripts/uninstallVeretino.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#remove Veretino from system:
+
+sudo rm /usr/share/applications/veretino.desktop
+sudo rm /usr/share/pixmaps/veretino.png
+sudo rm /usr/bin/veretino
+
+echo "All done..."
+exit
