17d321ae59c6bd9b8c90a166a9af3d624000a8ed
artemvlas <artemvlas@proton.me>
2023-04-30 11:50:40 +0300

    release 0.2.0

--- src/jsondb.cpp
+++ src/jsondb.cpp
@@ -49,7 +49,7 @@ void JsonDb::makeJson(const DataContainer &data)
     bool isWorkDirRelative = !data.metaData.databaseFileName.contains('/');
 
     QJsonObject header;
-    header["Created with"] = "Veretino dev_0.2.0 https://github.com/artemvlas/veretino";
+    header["Created with"] = "Veretino 0.2.0 https://github.com/artemvlas/veretino";
     header["Files number"] = data.metaData.numChecksums;
     header["Folder"] = paths::folderName(data.metaData.workDir);
     header["Used algorithm"] = QString("SHA-%1").arg(data.metaData.shaType);
