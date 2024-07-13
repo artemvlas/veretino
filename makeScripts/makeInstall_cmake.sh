43748eec754da95de1701de101e23aba39c42b4a
Artem V. <artemvlas@proton.me>
2024-07-13 11:10:30 +0300

    added CMake build system

---
+++ makeScripts/makeInstall_cmake.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# It is assumed that 'make' and 'cmake' are installed
+# sudo pacman -S base-devel cmake
+
+set -x
+set -e
+
+TEMP_BASE=/tmp
+
+REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" veretino-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+trap cleanup EXIT
+
+pushd "$BUILD_DIR"
+
+# make the project
+cmake "$REPO_ROOT" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+
+# build the application on all CPU cores
+make -j$(nproc)
+
+# install [/usr/bin/veretino]
+sudo make install
+
+echo "All done..."
