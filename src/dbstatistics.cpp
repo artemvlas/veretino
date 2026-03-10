9b2a7bb49a439e80a0433bd3bf771a03e8dbc079
Artem V. <artemvlas@proton.me>
2026-03-10 20:32:23 +0200

    [impl.] class DbStatistics

---
+++ src/dbstatistics.cpp
@@ -0,0 +1,96 @@
+#include "dbstatistics.h"
+#include "treemodeliterator.h"
+#include "pathstr.h"
+#include <QFileInfo>
+
+DbStatistics::DbStatistics() {}
+
+DbStatistics::DbStatistics(const DataContainer *data)
+    : m_data(data) {}
+
+void DbStatistics::setData(const DataContainer *data)
+{
+    m_data = data;
+    m_numbers.clear();
+}
+
+const Numbers& DbStatistics::updateNumbers()
+{
+    m_numbers = getNumbers();
+    return m_numbers;
+}
+
+Numbers DbStatistics::getNumbers(const QModelIndex &rootIndex) const
+{
+    if (!m_data)
+        return {};
+
+    Numbers num;
+
+    TreeModelIterator iter(m_data->m_model, rootIndex);
+
+    while (iter.hasNext()) {
+        iter.nextFile();
+        num.addFile(iter.status(), iter.size());
+    }
+
+    return num;
+}
+
+bool DbStatistics::contains(const FileStatuses flags, const QModelIndex &subfolder) const
+{
+    const Numbers &num = TreeModel::isFolderRow(subfolder) ? getNumbers(subfolder) : m_numbers;
+
+    return num.contains(flags);
+}
+
+bool DbStatistics::checkCondition(Condition condition) const
+{
+    if (!m_data)
+        return false;
+
+    switch (condition) {
+    case NoDbFile:
+    //case InCreation:
+        return (m_data->m_metadata.dbFileState == MetaData::NoFile);
+    case DbFileExists:
+        return QFileInfo::exists(m_data->m_metadata.dbFilePath);
+    case NotSaved:
+        return (m_data->m_metadata.dbFileState == DbFileState::NotSaved);
+    case Immutable:
+        return (m_data->m_metadata.flags & MetaData::FlagConst);
+    case WorkDirRelative:
+        return (m_data->m_metadata.workDir == pathstr::parentFolder(m_data->m_metadata.dbFilePath));
+    case FilterApplied:
+        return m_data->m_metadata.filter.isEnabled();
+    case AllChecked:
+        return (contains(FileStatus::CombChecked)
+                && !contains(FileStatus::CombNotChecked | FileStatus::CombProcessing));
+    case AllMatched:
+        return isAllMatched(m_numbers);
+    case HasPossiblyMovedItems:
+        return contains(FileStatus::New) && contains(FileStatus::Missing);
+    default:
+        break;
+    }
+
+    return false;
+}
+
+bool DbStatistics::isAllMatched(const QModelIndex &subfolder) const
+{
+    const Numbers &nums = TreeModel::isFolderRow(subfolder) ? getNumbers(subfolder) : m_numbers;
+
+    return isAllMatched(nums);
+}
+
+bool DbStatistics::isAllMatched(const Numbers &nums)
+{
+    return (!nums.contains(FileStatus::CombProcessing)
+            && nums.numberOf(FileStatus::Matched) == nums.numberOf(FileStatus::CombHasChecksum));
+}
+
+bool DbStatistics::isDbFileState(DbFileState state) const
+{
+    return m_data && (state == m_data->m_metadata.dbFileState);
+}
---
+++ src/dbstatistics.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Veretino,
+ * licensed under the GNU GPLv3.
+ * https://github.com/artemvlas/veretino
+*/
+#ifndef DBSTATISTICS_H
+#define DBSTATISTICS_H
+
+#include "datacontainer.h"
+#include "numbers.h"
+
+class DbStatistics
+{
+public:
+    DbStatistics();
+    explicit DbStatistics(const DataContainer *data);
+
+    void setData(const DataContainer *data);
+
+    Numbers getNumbers(const QModelIndex &rootIndex = QModelIndex()) const;
+    const Numbers& updateNumbers();
+
+    bool contains(const FileStatuses flags,
+                  const QModelIndex &subfolder = QModelIndex()) const;
+
+    bool isAllMatched(const QModelIndex &subfolder = QModelIndex()) const;
+    static bool isAllMatched(const Numbers &nums);
+
+    bool isDbFileState(DbFileState state) const;
+
+    enum Condition {
+        // DB
+        NoDbFile = 1 << 0,
+        InCreation = NoDbFile,
+        DbFileExists = 1 << 1,
+        NotSaved = 1 << 2,
+        Immutable = 1 << 3, // has FlagConst
+        WorkDirRelative = 1 << 4,
+        FilterApplied = 1 << 5,
+
+        // File Items
+        AllChecked = 1 << 6,
+        AllMatched = 1 << 7,
+        HasPossiblyMovedItems = 1 << 8, // has New and Missing items
+    };
+
+    bool checkCondition(Condition condition) const;
+
+    Numbers m_numbers;
+
+private:
+    const DataContainer *m_data = nullptr;
+};
+
+#endif // DBSTATISTICS_H
